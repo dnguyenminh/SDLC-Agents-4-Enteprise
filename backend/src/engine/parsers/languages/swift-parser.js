437e7986ffc064e436e54ee9f47a9529af4af2db
nguyenminh <dnguyenminh@gmail.com>
2026-09-08 23:02:59 +0700

    feat(parser): add Swift tree-sitter wasm and parser wiring

--- backend/src/engine/parsers/languages/swift-parser.js
+++ backend/src/engine/parsers/languages/swift-parser.js
@@ -1,4 +1,16 @@
-export default class SwiftParser {
-  constructor(parser, langId) { this.parser = parser; this.langId = langId; }
-  parse(source, filePath) { return { symbols: [], relationships: [] }; }
+import { GenericTreeSitterParser } from './generic-tree-sitter-parser.js';
+
+const SWIFT_NODE_MAP = {
+  function: ['function_declaration', 'func'],
+  class: ['class_declaration', 'class'],
+  struct: ['struct_declaration', 'struct'],
+  enum: ['enum_declaration', 'enum'],
+  protocol: ['protocol_declaration', 'protocol'],
+  variable: ['variable_declaration']
+};
+
+export default class SwiftParser extends GenericTreeSitterParser {
+  constructor(parser, langId) {
+    super(parser, langId, SWIFT_NODE_MAP);
+  }
 }
